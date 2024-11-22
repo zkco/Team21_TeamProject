70518b54432ea81925c444f8dde2748fed0bc633
leecoading <leehy011011@gmail.com>
2024-11-22 11:00:31 +0900

    [Update] GameOverPopup code
    플레이어의 hp가 0이되면 해당 팝업을 실행하게 playerCondition에서 조건문 추가.
    
    gameoverpopup실행 시 playerdie 소리 출력, retry 누를 시 해당 scene으로 다시 이동하게 수정.

--- Assets/Scripts/Player/PlayerCondition.cs
+++ Assets/Scripts/Player/PlayerCondition.cs
@@ -10,6 +10,7 @@ public class PlayerCondition : MonoBehaviour
     public Image Mana;
     public Image Exp;
     public TextMeshProUGUI level;
+    public bool isPlayerDead = false;
 
     private void Start()
     {
@@ -23,5 +24,20 @@ public class PlayerCondition : MonoBehaviour
         Mana.fillAmount = Mathf.Min((float)status.Mp / (float)status.MaxMp, 1f);
         Exp.fillAmount = Mathf.Min((float)status.Exp / (float)status.MaxExp, 1f);
         level.text = status.Lv.ToString();
+
+        PlayerDie();
+    }
+
+    private void PlayerDie()
+    {
+        if (isPlayerDead)
+            return;
+
+        if (Managers.PlayerManager.Player.Status.Hp <= 0)
+        {
+            Managers.SoundManager.PlaySFX(SFXType.playerDie);
+            isPlayerDead = true;
+            Managers.UIManager.CreateUI(UIType.GameOverPopup);
+        }
     }
 }
